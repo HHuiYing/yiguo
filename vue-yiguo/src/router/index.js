3dd0d71d7743458d2a09ab6a9476e1c025d10a17
guai-tongxue <1119362740@qq.com>
2020-08-24 09:16:34 +0800

    后台管理页面

--- vue-yiguo/src/router/index.js
+++ vue-yiguo/src/router/index.js
@@ -1,5 +1,18 @@
 import Vue from 'vue';
 
+import Home from '../pages/Home.vue'
+
+import User from '../pages/user/Default.vue'
+import UserList from '../pages/user/List.vue'
+import UserAdd from '../pages/user/Add.vue'
+import UserEdit from '../pages/user/Edit.vue'
+
+import Order from '../pages/Order.vue'
+import Goods from '../pages/Goods.vue'
+import Reg from '../pages/Reg.vue'
+import Login from '../pages/Login.vue'
+import NotFound from '../pages/NotFound.vue'
+
 // 1. 引入Vue-Router
 import VueRouter from 'vue-router'
 
@@ -11,7 +24,52 @@ Vue.use(VueRouter)
 // 3. 实例化并配置参数
 const router = new VueRouter({
     // mode:'history', // 一般上线后改为history路由(需要额外配置服务器)
-    routes: [{
+    routes: [
+        {
+            path: '/', // /->/home
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path: '/user',
+            component: User,
+            children: [
+                // 进入用户管理页面直接跳转到用户列表
+                {
+                    path: '',
+                    redirect: 'list'
+                },{
+                    path: 'add',
+                    component: UserAdd
+                }, {
+                    path: 'list',
+                    component: UserList
+                }, {
+                    name: 'ussrEdit',
+                    path: 'edit/:id',
+                    component: UserEdit
+                }]
+        },
+        {
+            path: '/order',
+            component: Order
+        },
+        {
+            path: '/goods',
+            component: Goods
+        },
+        {
+            path: '/reg',
+            component: Reg
+        },
+        {
+            path: '/login',
+            component: Login
+        },
+        {
             path: '/404',
             component: NotFound
         },
@@ -25,3 +83,4 @@ const router = new VueRouter({
 })
 
 export default router;
+console.log('router=', router);
