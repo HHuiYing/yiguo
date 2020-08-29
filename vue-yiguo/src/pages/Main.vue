5f1743dea762b93caf255368fbbd68868e0f4652
guai-tongxue <1119362740@qq.com>
2020-08-29 16:02:17 +0800

    0829pm

--- vue-yiguo/src/pages/Main.vue
+++ vue-yiguo/src/pages/Main.vue
@@ -1,79 +1,76 @@
 <template>
   <div>
     <h2 class="user-manage">首页</h2>
-    <el-col :span="10" class="line-chart">
+    <el-col :span="24" class="line-chart">
       <el-card shadow="hover" style="height:420px">
-        鼠标悬浮时显示
-      </el-card>
-    </el-col>
-    <el-col :span="10" class="line-chart">
-      <el-card shadow="hover" style="height:420px">
-        <div id="main" style="width: 500px;height: 400px;"></div>
+        <div id="main" style="height: 400px;"></div>
       </el-card>
     </el-col>
   </div>
 </template>
 
 <script>
-import echarts from 'echarts'
-    export default {
-        name: '',
-        data() {
-            return {
-                charts: '',
-            /*  opinion: ["1", "3", "3", "4", "5"],*/
-                opinionData: ["3", "2", "4", "4", "5"]
-            }
+import echarts from "echarts";
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  methods: {
+    drawLine() {
+      this.charts = echarts.init(document.getElementById("main"));
+      var option = {
+        tooltip: {
+          //设置tip提示
+          trigger: "axis",
         },
-        methods: {
-            drawLine(id) {
-                this.charts = echarts.init(document.getElementById(id))
-                this.charts.setOption({
-                    tooltip: {
-                        trigger: 'axis'
-                    },
-                    legend: {
-                        data: ['近七日收益']
-                    },
-                    grid: {
-                        left: '3%',
-                        right: '4%',
-                        bottom: '3%',
-                        containLabel: true
-                    },
- 
-                    toolbox: {
-                        feature: {
-                            saveAsImage: {}
-                        }
-                    },
-                    xAxis: {
-                        type: 'category',
-                        boundaryGap: false,
-                    data: ["1","2","3","4","5"]
-                    
-                    },
-                    yAxis: {
-                        type: 'value'
-                    },
- 
-                    series: [{
-                        name: '近七日收益',
-                        type: 'line',
-                        stack: '总量',
-                        data: this.opinionData
-                    }]
-                })
-            }
+        legend: {
+          data: ["水果销售", "肉类销售", "海鲜销售", "蔬菜销售"],
         },
-        //调用
-        mounted() {
-            this.$nextTick(function() {
-                this.drawLine('main')
-            })
-        }
-    }
 
+        xAxis: {
+          boundaryGap: false,
+          data: ["周一", "周二", "周三", "周四", "周五", "周六", "周日"],
+
+          type: "category",
+        },
+        yAxis: {
+          type: "value",
+        },
+
+        series: [
+          {
+            name: "水果销售",
+            type: "line",
+            data: [567, 980, 670, 732, 876, 921, 1021],
+          },
+          {
+            name: "肉类销售",
+            type: "line",
+            data: [654, 682, 891, 434, 890, 530, 710],
+          },
+          {
+            name: "海鲜销售",
+            type: "line",
+            data: [190, 1232, 801, 1254, 1190, 1330, 1310],
+          },
+          {
+            name: "蔬菜销售",
+            type: "line",
+            data: [1420, 1332, 1701, 1534, 1890, 1530, 920],
+          },
+        ],
+      };
+      this.charts.setOption(option);
+    },
+  },
+  //调用
+  mounted() {
+    this.$nextTick(function () {
+      this.drawLine("main");
+    });
+  },
+};
 </script>
 <style lang="scss" scoped>
 .user-manage {
@@ -88,8 +85,8 @@ import echarts from 'echarts'
   font-size: 18px;
   color: #008842;
 }
-.line-chart{
-  margin: 20px 50px;
+.line-chart {
+  padding: 20px;
   float: left;
 }
 </style>
