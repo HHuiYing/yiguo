a0f44a335e1afb39bea15694465a51e25c19012b
胡慧颖 <l>
2020-09-04 21:15:33 +0800

    Mine&Login页面更新

--- app-yiguo/src/views/Login.vue
+++ app-yiguo/src/views/Login.vue
@@ -1,10 +1,89 @@
 <template>
-  <div>
-    Login
+  <div class="loginWrap">
+    <!-- 头部 -->
+    <h2 class="headTop">登录</h2>
+    <!-- 登录框 -->
+    <div class="loginTable">
+      <van-form @submit="onSubmit" :model="ruleForm" :rules="rules">
+        <van-field v-model="ruleForm.username" name="用户名" label="用户名" label-width="50" />
+        <van-field
+          v-model="ruleForm.password"
+          type="password"
+          name="密码"
+          label="密码"
+          label-width="50"
+          :rules="[]"
+        />
+        <div class="errorMes"></div>
+        <div class="submitBtn">
+          <van-button round block type="info" native-type="submit">提交</van-button>
+        </div>
+      </van-form>
+    </div>
+    <!-- 背景图片 -->
+    <div class="backImg"></div>
+    <!-- 协议同意 -->
+    <div class="readMes">
+      <van-checkbox v-model="checked" shape="square"></van-checkbox>
+    </div>
   </div>
 </template>
 <script>
+import Vue from "vue";
+import { Form, Checkbox, Button, Field } from "vant";
+Vue.use(Form);
+Vue.use(Checkbox);
+Vue.use(Button);
+Vue.use(Field);
 export default {
-  name:'Login'
-}
+  name: "Login",
+  data() {
+    return {
+      checked: "",
+      ruleForm: {
+        username: "",
+        password: "",
+      },
+      rules: {
+        username: [{ required: true, message: "请填写用户名" }],
+        password: [{ required: true, message: "请填写密码" }],
+      },
+    };
+  },
+  methods: {
+    onSubmit(values) {
+      console.log("submit", values);
+    },
+  },
+};
 </script>
+<style lang="scss" scoped>
+.loginWrap {
+  height: 100%;
+  width: 100%;
+  .headTop {
+    width: 100%;
+    height: 40px;
+    background: #01b27a;
+    color: #ffffff;
+    font-size: 16px;
+    margin: 0;
+    line-height: 40px;
+    text-align: center;
+  }
+  .loginTable {
+    padding: 26px 22px 0;
+    .errorMes {
+      margin: 8px 0 0;
+      padding-left: 80px;
+      color: red;
+    }
+    .van-cell {
+      border-bottom: 1px solid #ccc;
+    }
+    .submitBtn {
+      margin-top: 30px;
+    }
+  }
+}
+</style>
--- app-yiguo/src/views/Mine.vue
+++ app-yiguo/src/views/Mine.vue
@@ -7,23 +7,90 @@
         <img src="../.././public/img/touxiang.jpg" alt />
       </div>
       <!-- 登录注册 -->
-      <p class="regLogin">登录/注册</p>
+      <p class="regLogin" @click="gotoLogin">登录/注册</p>
       <!-- 设置 -->
       <div class="topSet">
-        <van-icon name="setting-o" size="20" color="#ffffff"/>
+        <van-icon name="setting-o" size="20" color="#ffffff" />
       </div>
     </div>
     <!-- 卡包 -->
-    <div class="payCart"></div>
+    <ul class="payCart">
+      <li>
+        <span>-</span>
+        <em>卡包余额</em>
+      </li>
+      <li>
+        <span>-</span>
+        <em>悠币</em>
+      </li>
+    </ul>
     <!-- 订单 -->
-    <div class="orderCon"></div>
+    <div class="orderCon">
+      <van-grid :column-num="5" :border="false" icon-size="19px">
+        <van-grid-item icon="debit-pay" text="待支付" />
+        <van-grid-item icon="logistics" text="待收货" />
+        <van-grid-item class="sendCur" icon="smile-comment-o" text="评价送悠币" />
+        <van-grid-item icon="peer-pay" text="在线退换货" />
+        <van-grid-item icon="balance-list-o" text="全部订单 >" />
+      </van-grid>
+    </div>
     <!-- 信息内容 -->
-    <div class="myMessage"></div>
+    <div class="myMessage">
+      <van-grid :column-num="4" :border="false" icon-size="19px">
+        <van-grid-item v-for="(item,i) in menu" :key="i" :icon="item.icon" :text="item.text" />
+      </van-grid>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "Mine",
+  data() {
+    return {
+      recommend: [],
+      menu: [
+        {
+          text: "优惠券",
+          icon: "coupon",
+        },
+        {
+          text: "充值卡",
+          icon: "card",
+        },
+        {
+          text: "提货券",
+          icon: "send-gift",
+        },
+        {
+          text: "收货地址",
+          icon: "location",
+        },
+        {
+          text: "帮助中心",
+          icon: "question",
+        },
+        {
+          text: "我的团购",
+          icon: "bag",
+        },
+        {
+          text: "账户余额",
+          icon: "bill",
+        },
+        {
+          text: "在线客服",
+          icon: "service",
+        },
+      ],
+    };
+  },
+  components: {},
+  methods:{
+    //跳转到登陆页面
+    gotoLogin(){
+      this.$router.push("/Login");
+    }
+  },
 };
 </script>
 <style lang="scss" scoped>
@@ -43,7 +110,7 @@ export default {
       width: 76px;
       height: 76px;
       border-radius: 50%;
-      padding: 27px 0 0 20px;
+      margin: 27px 0 0 20px;
       float: left;
       > img {
         width: 100%;
@@ -64,6 +131,40 @@ export default {
       position: absolute;
       top: 10px;
       right: 16px;
+    }
+  }
+  .payCart {
+    display: flex;
+    width: 100%;
+    height: 30px;
+    padding: 7.7px 0;
+    justify-content: space-around;
+    align-items: center;
+    background: #fff;
+    > li {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      text-align: center;
+      font-size: 12px;
+      > span {
+        color: #0e9f4e;
+      }
+      > em {
+        font-style: normal;
+      }
+    }
+  }
+  .orderCon {
+    margin: 7px 0;
+    width: 100%;
+    /deep/.van-grid-item__text{
+      font-size: 9px; 
+    }
+  }
+  .myMessage{
+    .van-grid-item{
+      color: #ccc;
     }
   }
 }
